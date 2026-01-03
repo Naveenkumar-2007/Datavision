ce8246f1b58ec8c32c590e448b722b6b72a73ef2
Naveenkumar <naveenkumarchapala686@gmail.com>
2026-01-04 00:09:48 +0530

    Add pre-built frontend dist folder for HuggingFace deployment

---
+++ frontend/dist/assets/index-C_XbgJDe.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1afb07ea07897e28208034f96f64294052f0c55fc5ffd7d096c9f20b051da38
+size 6995189
---
+++ frontend/dist/assets/plotly.min-BpASUX0g.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:25ae3546bc5add70bfd677e33fc5bd4675905d0e07c6f0403b5798a73d76acc1
+size 4803181
