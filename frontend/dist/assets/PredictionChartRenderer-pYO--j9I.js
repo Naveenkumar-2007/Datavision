a7bdadc02686a3d5042d99ba1dd8c059b70b12eb
Naveenkumar <naveenkumarchapala686@gmail.com>
2026-01-04 16:02:04 +0530

    Force add frontend/dist assets to fix 404

---
+++ frontend/dist/assets/PredictionChartRenderer-pYO--j9I.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:09f0df7ff76d59d5398ee579fa9f4969b6e8419ffb9f56aba0474469d75eb05b
+size 31141
---
+++ frontend/dist/assets/index.es-Dsrs1flL.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86bed3aa4482fb931bfd9fc865b6e290364803f62cd359bc2f602226cb86715a
+size 150526
