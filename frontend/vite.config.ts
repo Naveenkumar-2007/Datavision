47d49d82ec952c87e7a0d6c00c10c8feb60f3b9b
Naveen-2007 <naveenkumar78987@gmail.com>
2026-07-12 11:39:10 +0530

    Fix Vite blank screen of death by adding node polyfill for buffer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
+  ],
   base: '/',
   envDir: '.',
   resolve: {
